4b95103b182b3773fe88e5203307263bd2d33bc3
Lxy564 <2933069534@qq.com>
2026-07-23 23:07:32 +0800

    Update app.vue

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,6 +1,4 @@
-<!-- src/App.vue -->
 <template>
-  <!-- 这一行代码至关重要！路由跳转的页面都在这里显示 -->
   <router-view />
 </template>
 
@@ -8,7 +6,6 @@
 </script>
 
 <style>
-/* 全局样式可以写在这里 */
 body {
   margin: 0;
   background: #f5f7fa;
